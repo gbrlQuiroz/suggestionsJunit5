6ad65ec82b49c79559db112aac3b9b08ad08339a
Gabriel Quiroz <gabriel@mb.company>
2021-10-09 18:49:01 -0500

    persistences

---
+++ src/main/java/com/challenge/suggestions/persistences/CityRepository.java
@@ -0,0 +1,17 @@
+package com.challenge.suggestions.persistences;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.NotNull;
+
+import java.util.List;
+import com.challenge.suggestions.models.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+
+    //buscar por campo 'name' con LIKE
+    List<City> findByNameContains(@NotNull String name);
+
+}
