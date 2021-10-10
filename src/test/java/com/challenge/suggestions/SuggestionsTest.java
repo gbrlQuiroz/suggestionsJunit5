754edfcb7f9c0245bc5f497a1669c882eb1b1714
Gabriel Quiroz <gabriel@mb.company>
2021-10-09 19:34:30 -0500

    primer test de registros en base de datos

--- src/test/java/com/challenge/suggestions/SuggestionsTest.java
+++ src/test/java/com/challenge/suggestions/SuggestionsTest.java
@@ -6,10 +6,26 @@ import org.junit.Test;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
 
+
+import com.challenge.suggestions.persistences.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class SuggestionsTest extends SuggestionsTestConfiguration{
-    
+public class SuggestionsTest extends SuggestionsTestConfiguration {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    /**
+     * Probar que se cargen todos los registros en la base en memoria
+     */
+    @Test
+    public void database() throws Exception {
+        long cuenta = cityRepository.count();
+        assertEquals(199, cuenta);
+        log.info("Se cuentan registros: {}", cuenta);
+    }
+
 }
