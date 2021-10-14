0b641f884f42d69ac83fadf9186a6d3b2c016046
gbrlQuiroz <orion_fire_qog@hotmail.com>
2021-10-14 13:03:09 -0500

    creando test con webTestClient

--- src/test/java/com/challenge/suggestions/SuggestionsTest.java
+++ src/test/java/com/challenge/suggestions/SuggestionsTest.java
@@ -8,8 +8,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 import com.challenge.suggestions.persistences.*;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -18,8 +18,11 @@ public class SuggestionsTest extends SuggestionsTestConfiguration {
     @Autowired
     private CityRepository cityRepository;
 
+
     /**
      * Probar que se cargen todos los registros en la base en memoria
+     * 
+     * mvn clean ; mvn test -Dtest=SuggestionsTest#database
      */
     @Test
     public void database() throws Exception {
@@ -27,31 +30,35 @@ public class SuggestionsTest extends SuggestionsTestConfiguration {
         assertEquals(7234, cuenta);
         log.info("Se cuentan registros: {}", cuenta);
     }
+
     /**
-     * Probar endpoint sin parametros de latitud y longitud
-     * en todos los elemento el campos 'score' de ser 1.0
+     * Probar endpoint sin parametros de latitud y longitud en todos los elemento el
+     * campos 'score' de ser 1.0
+     * 
+     * mvn clean ; mvn test -Dtest=SuggestionsTest#getSuggestionsWithout    
      */
     @Test
     public void getSuggestionsWithout() throws Exception {
-        mockMvc.perform(
-                MockMvcRequestBuilders.get("/suggestions?q=Am")
-                .contentType(JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        mockMvc.perform(MockMvcRequestBuilders.get("/suggestions?q=Am").contentType(JSON))
+                .andExpect(MockMvcResultMatchers.status()
+                .isOk())
                 .andDo(MockMvcResultHandlers.print());
 
         log.info("Se ejecuto: /suggestions?q=Am --->>> HTTP-status: 200(OK) --->>> Todos los  score son 1.0");
     }
 
     /**
-     * Probar endpoint con latitud y longitud
-     * el campo 'score' es asignado siguiendo las regla de negocio
+     * Probar endpoint con latitud y longitud el campo 'score' es asignado siguiendo
+     * las regla de negocio
+     * 
+     * mvn clean ; mvn test -Dtest=SuggestionsTest#getSuggestionsWithLatitudeAndLongitude
      */
     @Test
     public void getSuggestionsWithLatitudeAndLongitude() throws Exception {
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/suggestions?q=Am&latitude=45.00000&longitude=-75.00000")
-                .contentType(JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                MockMvcRequestBuilders.get("/suggestions?q=Am&latitude=45.00000&longitude=-75.00000").contentType(JSON))
+                .andExpect(MockMvcResultMatchers.status()
+                .isOk())
                 .andDo(MockMvcResultHandlers.print());
 
         log.info("Se ejecuto: /suggestions?q=Am&latitude=45.00000&longitude=-75.00000 --->>> HTTP-status: 200(OK)");
@@ -59,40 +66,46 @@ public class SuggestionsTest extends SuggestionsTestConfiguration {
 
     /**
      * Probar endpoint con un criterio exacto
+     * 
+     * mvn clean ; mvn test -Dtest=SuggestionsTest#getSuggestionsOnlyOne
      */
     @Test
     public void getSuggestionsOnlyOne() throws Exception {
-        String resultado = mockMvc.perform(
-            MockMvcRequestBuilders.get("/suggestions?q=Ajax")
-            .contentType(JSON))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andDo(MockMvcResultHandlers.print())
-            .andReturn().getResponse().getContentAsString();
+        String resultado = mockMvc.perform(MockMvcRequestBuilders.get("/suggestions?q=Ajax").contentType(JSON))
+                .andExpect(MockMvcResultMatchers.status()
+                .isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn()
+                .getResponse().getContentAsString();
 
         log.info("Se ejecuto: /suggestions?q=Ajax --->>> HTTP-status: 200(OK)");
 
         String cadena = "{\"suggestions\":[{\"name\":\"Ajax\",\"latitude\":\"43.85012\",\"longitude\":\"-79.03288\",\"score\":1.0}]}";
-        
+
         log.info("revisando el valor de latitude");
         assertEquals(true, resultado.contains("43.85012"));
-        
-        log.info("revisando que json corresponda a: {}",cadena);
+
+        log.info("revisando que json corresponda a: {}", cadena);
         assertEquals(cadena, resultado);
+
     }
 
     /**
-     * Probar con error, no se esta poniendo el parametro requerido 'q'
-     * regresa 400 bad request
+     * Probar con error, no se esta poniendo el parametro requerido 'q' regresa 400
+     * bad request
+     * 
+     * mvn clean ; mvn test -Dtest=SuggestionsTest#getSuggestionsError
      */
     @Test
     public void getSuggestionsError() throws Exception {
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/suggestions/?latitude=45.00000&longitude=-75.00000")
-                .contentType(JSON))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andDo(MockMvcResultHandlers.print());
+        MockMvcRequestBuilders.get("/suggestions/?latitude=45.00000&longitude=-75.00000").contentType(JSON))
+        .andExpect(MockMvcResultMatchers.status()
+        .isBadRequest())
+        .andDo(MockMvcResultHandlers.print());
 
         log.info("Se ejecuto: /suggestions/?latitude=45.00000&longitude=-75.00000 --->>> HTTP-status: 400(falta el parametro 'q')");
+
     }
 
 }
---
+++ src/test/java/com/challenge/suggestions/SuggestionsTestClient.java
@@ -0,0 +1,57 @@
+package com.challenge.suggestions;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+// import org.junit.jupiter.api.Test; //esta es la original, no corre en terminal
+import org.junit.Test;  //esta si corre en la terminal
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.challenge.suggestions.views.SuggestionView;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(locations = {"classpath:application-test.properties"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+// We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
+public class SuggestionsTestClient {
+
+    // Spring Boot will create a `WebTestClient` for you,
+    // already configure and ready to issue requests against "localhost:RANDOM_PORT"
+    @Autowired
+    private WebTestClient webTestClient;
+
+    /**
+     * Probar endpoint con un criterio exacto
+     * 
+     * mvn clean ; mvn test -Dtest=SuggestionsTestClient#getSuggestionsOnlyOne
+     */
+    @Test
+    public void getSuggestionsOnlyOne() {
+
+        webTestClient
+                .get()
+                .uri("/suggestions?q=Ajax")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(SuggestionView.class).value(suggestion -> {
+                    assertThat(suggestion.getSuggestions().toString())
+                    .isEqualTo("[CityView(name=Ajax, latitude=43.85012, longitude=-79.03288, score=1.0)]");
+                });
+    }
+}
